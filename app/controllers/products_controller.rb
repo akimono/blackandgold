b379302b3189f0f40a61351cef04ff0adeb17d4a
akimono <frank.cioppettini@gmail.com>
2015-05-08 22:36:47 -0400

    fix" ;

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,12 @@ class ProductsController < ApplicationController
       format.html # index.html.erb
       format.json {render json: @product}
   end
+  end
   def new
   	   @product = Product.new
        respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @product }
+  end
   end
 end
