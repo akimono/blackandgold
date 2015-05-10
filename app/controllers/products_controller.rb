6433c9c24e4d52dde4048cac074e6e575ed7768a
akimono <frank.cioppettini@gmail.com>
2015-05-09 23:25:03 -0400

    dsfljk

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
      	 if @product.save
-        format.html { redirect_to :controller => 'products', :action => 'show', :id => @product.id }
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
       	else
         format.html { render action: "new" }
       	end
