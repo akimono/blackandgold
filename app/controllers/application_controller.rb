5544d55f01a860444d11b6eb10bfcbc32d4b74fe
akimono <frank.cioppettini@gmail.com>
2015-05-11 12:18:45 -0400

    curcart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  def @cart
+  def currentcart
   	if (session[:cart] = nil )
   		@cart = nil
   	else
   		@cart = session[:cart]
   		cookies[:cart] = @cart
-end
+	end
 end
